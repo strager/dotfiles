strager/dotfiles

commit 756ba27bd209d81891b0a4c5805724beb15504e9

[vim] Support clangd go-to-definition with a ctags fallback
* Implement <C-]> using vim-lsp's :LspDefinition command, falling back
  to Vim's built-in <C-]> command if a definition isn't found.
* Integrate clangd with vim-lsp, automatically finding the compilation
  database and project root.

These two features lets me easily switch between clangd-based projects
and ctags-based projects.

vim/vim/after/compiler/gcc.vim
@@ -1,0 +1,9 @@
+" HACK(strager): vim-lsp's interface is so confusing. Instead of just giving
+" vim-lsp the config, we must hand vim-lsp a callback (au User lsp_setup) so we
+" can hand vim-lsp a callback (lsp#register_server()) which returns the config.
+" Otherwise, lsp#disable() followed by lsp#enable() doesn't re-enable the server
+" properly.
+augroup strager_compiler_gcc
+  au!
+  au User lsp_setup call strager#lsp#register_clangd_server()
+augroup END

vim/vim/autoload/strager/.gitignore
@@ -1,0 +1,1 @@
+/test_tag_helper.c.tags

.gitmodules
@@ -19,3 +19,9 @@
 [submodule "vim/vim/bundle/fzf"]
 	path = vim/vim/bundle/fzf
 	url = https://github.com/junegunn/fzf
+[submodule "vim/vim/bundle/async.vim"]
+	path = vim/vim/bundle/async.vim
+	url = https://github.com/prabirshrestha/async.vim.git
+[submodule "vim/vim/bundle/vim-lsp"]
+	path = vim/vim/bundle/vim-lsp
+	url = https://github.com/prabirshrestha/vim-lsp.git

vim/vim/after/ftplugin/c.vim
@@ -1,0 +1,1 @@
+compiler gcc

vim/vim/after/ftplugin/cpp.vim
@@ -1,0 +1,1 @@
+compiler gcc

vim/vim/after/ftplugin/objc.vim
@@ -1,0 +1,1 @@
+compiler gcc

vim/vim/after/ftplugin/objcpp.vim
@@ -1,0 +1,1 @@
+compiler gcc

vim/vim/autoload/strager/lsp.vim
@@ -1,0 +1,21 @@
+function strager#lsp#register_clangd_server()
+  let l:project_info = strager#project#find_c_project({
+    \ 'buffer_path': expand('%'),
+    \ 'cwd': getcwd(),
+  \ })
+
+  let l:command = ['clangd']
+  let l:compile_commands_path = l:project_info['compile_commands_path']
+  if l:compile_commands_path !=# v:none
+    call add(l:command, '-compile-commands-dir='.fnamemodify(l:compile_commands_path, ':h'))
+  endif
+
+  " FIXME(strager): How can we register the server only for the current buffer?
+  " vim-lsp's interface is so confusing.
+  call lsp#register_server({
+    \ 'cmd': {_ -> l:command},
+    \ 'name': 'strager#lsp# clangd '.l:project_info['source_path'],
+    \ 'root_uri': {_ -> lsp#utils#path_to_uri(l:project_info['source_path'])},
+    \ 'whitelist': ['c', 'cpp', 'objc', 'objcpp'],
+  \ })
+endfunction

commit 64438317411abed424ae2692e0a83a762472def5

[vim] Fix 'errorformat for gcc compiler
Commit 756ba27b caused Vim to use the gcc compiler's 'errorformat
setting instead of the default 'errorformat setting. The gcc compiler's
'errorformat is buggy, causing Vim to jump to the wrong file when
running 'ninja -C foo' via :make, for example. Work around the bug by
hacking 'errorformat.

vim/test.sh
@@ -52,6 +52,7 @@ log_and_run() {
 }
 
 run_vim_test --need-vimrc vim/vim/autoload/strager/test_tag.vim
+run_vim_test --need-vimrc vim/vim/test/test_c_make_ninja.vim
 run_vim_test --need-vimrc vim/vim/test/test_format.vim
 run_vim_test vim/vim/autoload/strager/test_file.vim
 run_vim_test vim/vim/autoload/strager/test_path.vim

vim/vim/after/compiler/gcc.vim
@@ -7,3 +7,9 @@ augroup strager_compiler_gcc
   au!
   au User lsp_setup call strager#lsp#register_clangd_server()
 augroup END
+
+" HACK(strager): Fix broken scan codes in Vim's gcc.vim's 'errorformat.
+" Replace: %D%*\a[%*\d]: Entering directory [`']%f
+"    with: %D%*\a[%*\d]: Entering directory %[`']%f
+" FIXME(strager): This should respect :CompilerSet but doesn't.
+let &errorformat = substitute(&errorformat, 'directory \[`''\]', 'directory %[`'']', 'g')

vim/vim/test/test_c_make_ninja.vim
@@ -1,0 +1,61 @@
+let s:script_path = expand('<sfile>:p')
+let s:script_dir_path = fnamemodify(s:script_path, ':h')
+
+function! Test_ninja_cd_compile_error_from_empty_buffer()
+  call s:set_up()
+  set makeprg=ninja
+  cd test_c_make_ninja_helper
+  silent make
+  call s:assert_cursor_should_have_jumped_to_error()
+endfunction
+
+function! Test_ninja_cd_compile_error_from_source_buffer()
+  call s:set_up()
+  set makeprg=ninja
+  cd test_c_make_ninja_helper
+  edit program.c
+  silent make
+  call s:assert_cursor_should_have_jumped_to_error()
+endfunction
+
+function! Test_ninja_relative_compile_error_from_empty_buffer()
+  call s:set_up()
+  set makeprg=ninja\ -C\ test_c_make_ninja_helper
+  silent make
+  call s:assert_cursor_should_have_jumped_to_error()
+endfunction
+
+function! Test_ninja_relative_compile_error_from_source_buffer()
+  call s:set_up()
+  echomsg &efm
+  set makeprg=ninja\ -C\ test_c_make_ninja_helper
+  edit test_c_make_ninja_helper/program.c
+  echomsg &efm
+  silent make
+  call s:assert_cursor_should_have_jumped_to_error()
+endfunction
+
+function! s:set_up()
+  cd `=s:script_dir_path`
+  %bwipeout
+endfunction
+
+function! s:assert_cursor_should_have_jumped_to_error()
+  let [l:_buffer_number, l:line, l:column, l:_offset, l:_curswant] = getcurpos()
+  let l:buffer_name = bufname('')
+  call assert_notequal('', l:buffer_name)
+  if l:buffer_name !=# ''
+    call assert_equal(
+      \ strager#path#join([
+        \ s:script_dir_path,
+        \ 'test_c_make_ninja_helper',
+        \ 'program.c',
+      \ ]),
+      \ fnamemodify(l:buffer_name, ':p'),
+    \ )
+    call assert_equal(3, l:line)
+    call assert_equal(10, l:column)
+  endif
+endfunction
+
+call strager#test#run_all_tests()

vim/vim/test/test_c_make_ninja_helper/.gitignore
@@ -1,0 +1,1 @@
+/program

vim/vim/test/test_c_make_ninja_helper/.ninja_log
@@ -1,0 +1,1 @@
+# ninja log v5

vim/vim/test/test_c_make_ninja_helper/build.ninja
@@ -1,0 +1,9 @@
+ninja_required_version = 1.3
+
+cc = cc
+cflags =
+
+rule c_compile
+  command = $cc $cflags -o $out -- $in
+
+build program: c_compile program.c

vim/vim/test/test_c_make_ninja_helper/program.c
@@ -1,0 +1,4 @@
+int main() {
+  // Produce a compilation error by refering to the undefined variable 'x'.
+  return x;
+}