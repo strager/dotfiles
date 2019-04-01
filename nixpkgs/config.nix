dfe879bdbab8d3595331aceb929f79949a148aa2
Matthew Glazar <strager.nds@gmail.com>
2019-04-01 00:37:51 -0700

    [fzf] Upgrade fzf to 0.17.5
    Instead of referencing my fork of fzf in Nixpkgs, apply just my patch
    atop the latest version of fzf already in Nixpkgs. This means I get fzf
    upgrades for free (ideally).
    
    The fzf Vim plugin is still out of date and doesn't get upgrades for
    free.

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,8 +1,16 @@
 {
   packageOverrides = pkgs: {
     fzf = pkgs.fzf.overrideAttrs (attrs: {
-      # TODO(strager): Move fzf outside the vim directory.
-      src = ../vim/vim/pack/external/start/fzf;
+      patchPhase = ''
+        ${attrs.patchPhase}
+        patchPhase
+      '';
+      patches = (attrs.patches or []) ++ [
+        (pkgs.fetchpatch {
+          url = https://github.com/strager/fzf/commit/e2baf624790bed56f233433236c03b81f313ba51.patch;
+          sha256 = "1bg42l1fr6236ss03m2g9g2904rwv4f1w4crr7a6pvw8m2hi4a1j";
+        })
+      ];
     });
   };
 }
