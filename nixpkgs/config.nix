7aed96e4f752bc1a21602c4203fb2f733d987f07
Matthew Glazar <strager.nds@gmail.com>
2019-09-02 13:00:22 -0700

    [nix][vim] Work around bug in Vim
    See comment in nixpkgs/config.nix.
    
    Also, ensure Travis CI installs our specified version of Vim by running
    install.sh before running install-packages.

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -20,5 +20,16 @@
         })
       ];
     });
+
+    vim = pkgs.vim.overrideAttrs (attrs: rec {
+      # Work around quickfix bug in Vim v8.1.1547 (fixed in v8.1.1549).
+      version = "8.1.1546";
+      src = pkgs.fetchFromGitHub {
+        owner = "vim";
+        repo = "vim";
+        rev = "v${version}";
+        sha256 = "01sj4wwcxfqvm5ijh33v4m8kx08p2kabqnqgwc0ym7bc52r6yliw";
+      };
+    });
   };
 }
