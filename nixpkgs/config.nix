a25e375e2849b5c22785f9b9f5bd65a0d7cac5a1
Matthew Glazar <strager.nds@gmail.com>
2019-12-20 14:46:49 -0800

    [fzf] Upgrade to version 0.19
    fzf version 0.19 implemented my --no-progress feature, calling it
    --info=hidden (or --no-info). Use upstream's feature, dropping our
    patches.
    
    Our fzf patches don't apply cleanly on fzf version 0.19's sources.
    Dropping our patches should fix installing fzf with the latest Nixpkgs,
    fixing install-packages errors on Travis CI.

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -8,19 +8,6 @@
       ];
     });
 
-    fzf = pkgs.fzf.overrideAttrs (attrs: {
-      patchPhase = ''
-        ${attrs.patchPhase}
-        patchPhase
-      '';
-      patches = (attrs.patches or []) ++ [
-        (pkgs.fetchpatch {
-          url = https://github.com/strager/fzf/commit/e2baf624790bed56f233433236c03b81f313ba51.patch;
-          sha256 = "1bg42l1fr6236ss03m2g9g2904rwv4f1w4crr7a6pvw8m2hi4a1j";
-        })
-      ];
-    });
-
     vim = pkgs.vim.overrideAttrs (attrs: rec {
       # Work around quickfix bug in Vim v8.1.1547 (fixed in v8.1.1549).
       version = "8.1.1546";
