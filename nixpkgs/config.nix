748d663210d3eb017a0827085b04a57547166177
Matthew Glazar <strager.nds@gmail.com>
2020-04-02 22:26:45 -0700

    [nix] Drop duplicity patch; allow upgrade to 0.8

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -2,12 +2,6 @@
   android_sdk.accept_license = true;
 
   packageOverrides = pkgs: {
-    duplicity = pkgs.duplicity.overrideAttrs (attrs: {
-      patches = (attrs.patches or []) ++ [
-        ./duplicity-performance.patch
-      ];
-    });
-
     vim = pkgs.vim.overrideAttrs (attrs: rec {
       # Work around quickfix bug in Vim v8.1.1547 (fixed in v8.1.1549).
       version = "8.1.1546";
