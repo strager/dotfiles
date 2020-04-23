8e413a493dff1d341839a45a625e89cb04f3b58c
Matthew Glazar <strager.nds@gmail.com>
2020-04-22 21:55:59 -0700

    [nixpkgs] Drop Vim version pin; upgrade to Vim 8.2

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -3,14 +3,6 @@
 
   packageOverrides = pkgs: {
     vim = pkgs.vim.overrideAttrs (attrs: rec {
-      # Work around quickfix bug in Vim v8.1.1547 (fixed in v8.1.1549).
-      version = "8.1.1546";
-      src = pkgs.fetchFromGitHub {
-        owner = "vim";
-        repo = "vim";
-        rev = "v${version}";
-        sha256 = "01sj4wwcxfqvm5ijh33v4m8kx08p2kabqnqgwc0ym7bc52r6yliw";
-      };
       buildInputs = attrs.buildInputs ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
         pkgs.xorg.libX11
         pkgs.xorg.libXext
