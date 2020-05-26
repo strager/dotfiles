10e62c2d086142512a4004d49f82fdfcedcbc085
Matthew Glazar <strager.nds@gmail.com>
2020-05-25 18:26:05 -0700

    [vim] Fix broken throwpoints for lambdas
    Vim patch 8.2.0499 broke throwpoints for lambdas. This causes some tests
    in vim/vim/autoload/strager/test_exception.vim to fail:
    
        [ RUN      ] Test_parse_throwpoint_from_dead_lambda
        Error detected while processing function strager#test#run_all_tests[11]..strager#test#run_tests:
        line   23:
        function strager#test#run_all_tests[11]..strager#test#run_tests[10]..Test_parse_throwpoint_from_dead_lambda line 13: Lambda frame should not have a name: Expected v:none but got 'Test_parse_throwpoint_from_dead_lambda'
        function strager#test#run_all_tests[11]..strager#test#run_tests[10]..Test_parse_throwpoint_from_dead_lambda line 18: Lambda frame should not have a source location: Expected v:none but got '/home/strager/Projects/dotfiles/vim/vim/autoload/strager/test_exception.vim'
        function strager#test#run_all_tests[11]..strager#test#run_tests[10]..Test_parse_throwpoint_from_dead_lambda line 23: Lambda frame should not have a source location: Expected v:none but got 107
        [  FAILED  ] Test_parse_throwpoint_from_dead_lambda
        [ RUN      ] Test_parse_throwpoint_from_live_lambda
        function strager#test#run_all_tests[11]..strager#test#run_tests[10]..Test_parse_throwpoint_from_live_lambda line 12: Lambda frames should not have a name: Expected v:none but got 'Test_parse_throwpoint_from_live_lambda'
        function strager#test#run_all_tests[11]..strager#test#run_tests[10]..Test_parse_throwpoint_from_live_lambda line 18: Expected 83 but got 85
        [  FAILED  ] Test_parse_throwpoint_from_live_lambda
    
    Upgrade Vim to include patch 8.2.0823 which fixes this issue.

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -3,6 +3,15 @@
 
   packageOverrides = pkgs: {
     vim = pkgs.vim.overrideAttrs (attrs: rec {
+      # Work around throwpoint bug in Vim v8.2.0499 (fixed in v8.2.0823).
+      version = "8.2.0823";
+      src = pkgs.fetchFromGitHub {
+        owner = "vim";
+        repo = "vim";
+        rev = "v${version}";
+        sha256 = "104wzl434scr43wdranch6jkpyxkxk27fs0lc4npk23gsjf46nld";
+      };
+
       buildInputs = attrs.buildInputs ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
         pkgs.xorg.libX11
         pkgs.xorg.libXext
