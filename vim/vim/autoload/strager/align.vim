strager/dotfiles

commit 376d61059593dc04e35b2a4df82b019a95a7208a

[vim] Refactor: make function autoload

vim/vim/autoload/strager/align.vim
@@ -1,0 +1,15 @@
+""" http://vim.wikia.com/wiki/Regex-based_text_alignment
+function! strager#align#align_section(regex) range
+  let extra = 1
+  let sep = empty(a:regex) ? '=' : a:regex
+  let maxpos = 0
+  let section = getline(a:firstline, a:lastline)
+  for line in section
+    let pos = match(line, ' *'.sep)
+    if maxpos < pos
+      let maxpos = pos
+    endif
+  endfor
+  call map(section, 'AlignLine(v:val, sep, maxpos, extra)')
+  call setline(a:firstline, section)
+endfunction

vim/vimrc
@@ -45,30 +45,7 @@ set clipboard=
 
 """ Text alignment
 """ http://vim.wikia.com/wiki/Regex-based_text_alignment
-command! -nargs=? -range Align <line1>,<line2>call AlignSection('<args>')
-function! AlignSection(regex) range
-  let extra = 1
-  let sep = empty(a:regex) ? '=' : a:regex
-  let maxpos = 0
-  let section = getline(a:firstline, a:lastline)
-  for line in section
-    let pos = match(line, ' *'.sep)
-    if maxpos < pos
-      let maxpos = pos
-    endif
-  endfor
-  call map(section, 'AlignLine(v:val, sep, maxpos, extra)')
-  call setline(a:firstline, section)
-endfunction
-
-function! AlignLine(line, sep, maxpos, extra)
-  let m = matchlist(a:line, '\(.\{-}\) \{-}\('.a:sep.'.*\)')
-  if empty(m)
-    return a:line
-  endif
-  let spaces = repeat(' ', a:maxpos - strlen(m[1]) + a:extra)
-  return m[1] . spaces . m[2]
-endfunction
+command! -nargs=? -range Align <line1>,<line2>call strager#align#align_section('<args>')
 
 " Make Y consistent with D (i.e. D : d$ :: Y : y$)
 nmap <unique> Y y$

commit ae155fbc140ed40e2303aee8fe34842cc35dda57

[vim] Fix :Align
Commit 376d61059593dc04e35b2a4df82b019a95a7208a broke :Align. Fix it.

vim/vim/autoload/strager/align.vim
@@ -10,6 +10,15 @@ function! strager#align#align_section(regex) range
       let maxpos = pos
     endif
   endfor
-  call map(section, 'AlignLine(v:val, sep, maxpos, extra)')
+  call map(section, {_, val -> s:AlignLine(val, sep, maxpos, extra)})
   call setline(a:firstline, section)
+endfunction
+
+function! s:AlignLine(line, sep, maxpos, extra)
+  let m = matchlist(a:line, '\(.\{-}\) \{-}\('.a:sep.'.*\)')
+  if empty(m)
+    return a:line
+  endif
+  let spaces = repeat(' ', a:maxpos - strlen(m[1]) + a:extra)
+  return m[1] . spaces . m[2]
 endfunction