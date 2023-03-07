cd175a45a1fa0a2cd75ed02439e905db6c4aac4c
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-03-06 20:00:35 -0800

    [sapling] Make 'sl metaedit' easier to use

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -25,6 +25,12 @@
       };
     };
 
+    sapling = pkgs.sapling.overrideAttrs (attrs: {
+      patches = (attrs.patches or []) ++ [
+        ./sapling-metaedit-annoyance.patch
+      ];
+    });
+
     twitch-chat-downloader = pkgs.twitch-chat-downloader.overrideAttrs (attrs: {
       src = pkgs.fetchFromGitHub {
         owner = "PetterKraabol";
