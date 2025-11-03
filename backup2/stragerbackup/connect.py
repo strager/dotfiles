9fad9b92e975f4937ff34fa9b5a18be2df02acf7
strager <s@strager.net>
2025-11-02 22:46:50 -0500

    [backup2] Make archive and replicate not require password
    Use --persist-password when connecting to the repository.

---
+++ backup2/stragerbackup/connect.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import contextlib
+import datetime
+import json
+import logging
+import os
+import pathlib
+import pipes
+import platform
+import stragerbackup.site
+import stragerbackup.util
+import subprocess
+import sys
+import typing
+import unittest
+
+logger = logging.getLogger(__name__)
+
+
+def main() -> None:
+    logging.basicConfig(format="%(asctime)s %(message)s", level=logging.INFO)
+
+    site = stragerbackup.site.get_site_for_current_machine()
+    logger.info("connecting to local repository for site %s", site.name)
+
+    command = [
+        "kopia",
+        "repository",
+        "connect",
+        "filesystem",
+        "--persist-credentials",
+        "--path",
+        site.local_directory,
+    ]
+    logger.info(f"$ {stragerbackup.util.command_string(command)}")
+    subprocess.check_call(command)
+
+
+if __name__ == "__main__":
+    main()
